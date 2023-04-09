0cb9946e6ae73398c1d5365e53e634223ddbda20
Libor Miller <78346559+libormiller@users.noreply.github.com>
2023-04-09 20:12:45 +0200

    created api endpoint

--- src/routes/api/posts/+server.ts
+++
@@ -1,6 +0,0 @@
-
-export const GET = ({request}) => {
-    const autHeader = request.headers.get('Authorization')
-    console.log(autHeader)
-    return new Response(JSON.stringify({message: "hi"}), {status: 200});
-}
