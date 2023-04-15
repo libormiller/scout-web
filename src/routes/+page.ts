97144c83bf6dfa790a355febf60cfb81df44e4a1
Libor Miller <libor.miller@gmail.com>
2023-04-15 20:50:03 +0200

    First builded version

--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-import { pb } from '$lib/pocketbase'
-
-export const load = (async () => {
-    const records = await pb.collection('pictures').getFullList();
-    return {records};
-}) satisfies PageLoad;
