c65816ae00cdd61dcb004e92570625d33e4e7bf0
Libor Miller <78346559+libormiller@users.noreply.github.com>
2023-05-30 20:03:49 +0200

    working api with base64

--- src/routes/api/dataFromDevices/+server.ts
+++ src/routes/api/dataFromDevices/+server.ts
@@ -3,115 +3,58 @@ import { pb } from "$lib/pocketbase"
 import { POCKETBASE_API_ID, POCKETBASE_API_PASSWD } from '$env/static/private';
 
 //definice formátu pro data přicházející z gatewaye
-type jsonBodyFromGateway = {
+
+
+type messageFromDevice = {
     rcvTime: number;
-    srcIP: string;
-    srcPort: number;
-    Bytes: number;
-    payload: string;
+    imsi: number;
+    payload: payloadData;
 };
 
 type payloadData = {
     ID: number;
-    GNGLL: GNGLLmessage;
+    long: number;
+    lat: number;
     battery: number;
     temperature: number;
 }
 
-type GNGLLmessage = {
-    header: string,
-    lat: number,
-    latDir: string,
-    lon: number,
-    lonDir: string,
-    utc: number,
-    dataStatus: string,
-    modeInd: string
-}
-
-function hex2bin(hex){
-    return (parseInt(hex, 16).toString(2)).padStart(8, '0');
-}
-
-function base64ToHex(str) {
-    const raw = atob(str);
-    let result = '';
-    for (let i = 0; i < raw.length; i++) {
-      const hex = raw.charCodeAt(i).toString(16);
-      result += (hex.length === 2 ? hex : '0' + hex);
-    }
-    return result.toUpperCase();
-  }
-
 
 //definice pro http metodu POST
 export const POST = async ({ request }) => {
-    const authData = await pb.collection('users').authWithPassword(POCKETBASE_API_ID, POCKETBASE_API_PASSWD );
-    let payloadFromDevice = {} as payloadData;
 
-    let deviceData: jsonBodyFromGateway = await request.json()
-    let arrayOfBytesBIN: number[] = [];
-    let arrayOfGNGLL: string[] = [];
-    let helperHexToBin: string = "";
+    const authData = await pb.collection('users').authWithPassword(POCKETBASE_API_ID, POCKETBASE_API_PASSWD);
+    let rawDeviceData = await request.json()
+    let arrOfDecodedData = Buffer.from(rawDeviceData.payload, 'base64').toString('utf8').split(',')
 
-    //base64 na hex
-    deviceData.payload = base64ToHex(deviceData.payload)
-    console.log(deviceData.payload)
+    let deviceData: messageFromDevice = {
+        rcvTime: rawDeviceData.rcvTime,
+        imsi: rawDeviceData.srcImsi,
+        payload: {
+            ID: Number(arrOfDecodedData[0]),
+            long: Number(arrOfDecodedData[2]),
+            lat: Number(arrOfDecodedData[1]),
+            battery: Number(arrOfDecodedData[3]),
+            temperature: Number(arrOfDecodedData[4])
 
-    //hex na bin
-    deviceData.payload = hex2bin(deviceData.payload)
-    console.log(deviceData.payload)
-
-    /* roztřízení dat po bytech do pole */
-    const arrayOfBytesString = deviceData.payload.match(/.{1,8}/g)
-
-    /*    převod dat v poli z formátu string na číslo  */
-    arrayOfBytesString?.forEach(function (value: string) {
-        arrayOfBytesBIN.push(Number("0b".concat(value)))
-    })
-
-    //vložení dat z pole do proměnných
-    payloadFromDevice.ID = arrayOfBytesBIN[0];
-    payloadFromDevice.battery = arrayOfBytesBIN[50];
-    payloadFromDevice.temperature = arrayOfBytesBIN[51];
-
-    //odstranění uložených dat z původního pole
-    arrayOfBytesBIN.splice(51, 1);
-    arrayOfBytesBIN.splice(50, 1);
-    arrayOfBytesBIN.splice(0, 1);
-
-    //rozkódování čísel na znaky ASCII
-    arrayOfBytesBIN.forEach(function (value: number) {
-        arrayOfGNGLL.push(String.fromCharCode(value))
-    })
-
-    //rozdělení GNGLL zprávy do pole
-    const GNGLLmessageparsedByComma = arrayOfGNGLL.join("").split(",");
-
-    const record = await pb.collection('devices').getFullList( 1, { filter: 'deviceID = '.concat(payloadFromDevice.ID.toString()) } );
-    console.log(GNGLLmessageparsedByComma)
-
-    //vložení GNGLL zprávy do náležitých proměnných
-    payloadFromDevice.GNGLL = {
-        header: GNGLLmessageparsedByComma[0],
-        lat: Number(GNGLLmessageparsedByComma[1].replace(".", "")) / 1000000,
-        latDir: GNGLLmessageparsedByComma[2],
-        lon: Number(GNGLLmessageparsedByComma[3].replace(".", "")) / 1000000,
-        lonDir: GNGLLmessageparsedByComma[4],
-        utc: Number(GNGLLmessageparsedByComma[5]),
-        dataStatus: GNGLLmessageparsedByComma[6],
-        modeInd: GNGLLmessageparsedByComma[7]
+        }
     }
 
-    
+    console.log(deviceData)
+
+    const record = await pb.collection('devices').getFullList( 1, { filter: 'deviceID = '.concat(deviceData.payload.ID.toString()) } );
+
+
+
     //Data ve formátu ve kterém se uloží do databáze
+
     const data = {
         "device": record[0].id,
-        "latitude": payloadFromDevice.GNGLL.lat,
-        "longitude": payloadFromDevice.GNGLL.lon,
-        "bat": payloadFromDevice.battery,
+        "latitude": deviceData.payload.lat,
+        "longitude": deviceData.payload.long,
+        "bat": deviceData.payload.battery,
         "time": deviceData.rcvTime,
-        "temp": payloadFromDevice.temperature,
+        "temp": deviceData.payload.temperature,
     };
 
 
@@ -127,5 +70,4 @@ export const POST = async ({ request }) => {
     }
 
 
-    return new Response(undefined, { status: 200 })
 }
