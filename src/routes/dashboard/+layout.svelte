8f3faf4800c7c45cbd1236b3d9293f407c0ae161
Libor Miller <libor.miller@gmail.com>
2024-06-04 21:52:38 +0200

    Gramatická úprava

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -55,7 +55,7 @@ onMount(async function () {
     <!-- Odkaz na dynamické stránky které se generují z dat pro každé načtené zařízení -->
     <a data-sveltekit-reload href="/dashboard/map/{device.deviceID}">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"/></svg>
-       Zařísení s ID: {device.deviceID}
+       Zařízení s ID: {device.deviceID}
     </a>
   </li>
   {/each}
